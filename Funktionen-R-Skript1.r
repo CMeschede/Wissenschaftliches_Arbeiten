d742943928ab92da461ca3cc8fe44cfbcb330545
minhhongoc <94540091+minhhongoc@users.noreply.github.com>
2022-02-28 19:11:05 +0100

    Update Funktionen-R-Skript1.r

--- Funktionen-R-Skript1.r
+++ Funktionen-R-Skript1.r
@@ -60,8 +60,9 @@ functionE <- function(vector){
 }
 
 #Funktion F erstellt eine geeignete Visualisierung von drei oder vier kategorialen Variablen
-library(ggplot2) 
+library(ggplot2) =
+#Histogramm erstellen
 functionF <- function(daten) {
-   ggplot(daten, aes(x=reorder(data, data, function(x)-length(x)))) + geom_bar(fill='blue') +  labs(x='Visualisierung')
+   ggplot(daten, aes(x=reorder(data, data, function(x)-length(x)))) + geom_bar(fill='blue') +  labs(x = 'Histogram of Data)
 }                            
 
